59a43a825349fcb950ca7284bdc897eccd7d14ee
Student19 <apiwat@hotmail.com>
2017-06-12 14:23:28 +0700

    Variable

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//This Coment หรือ นี่คือ
+//นี่คือการประกาศตัวแปร แบบ ไม่สามารถเปลี่ยนค่าได้
+let pir = 3.14
+//pir = aa cannot_change let
+
+// นี่คือการประกาศตัวแปร แบบ เปลี่ยนค่าได้
+var intNumber = 123 //Implicit Variable inNumber = 123
+intNumber = 456
+
+//Explicit
+let myConstant1: Int = 123
